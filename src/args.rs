3216bdc795e01083d897104eb8cf706a50c9c816
LordFoom <lordfoom@gmail.com>
2026-03-01 21:18:22 +0200

    Add problem ids as param
    Intention is to allow triggering of specific code

--- src/args.rs
+++ src/args.rs
@@ -2,6 +2,9 @@ use clap::{Parser, ValueEnum};
 #[derive(Debug, Clone, ValueEnum)]
 pub enum ProblemId {
     CountElemGtPriorAvg,
+    Problem1,
 }
 #[derive(Parser, Debug)]
-pub struct AppArgs {}
+pub struct AppArgs {
+    pub problem_ids: Vec<ProblemId>,
+}
