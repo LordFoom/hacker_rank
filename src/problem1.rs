84d72d173201699153a3b5549fc35f043ae318bc
LordFoom <lordfoom@gmail.com>
2026-03-01 21:17:49 +0200

    Stub methods

---
+++ src/problem1.rs
@@ -0,0 +1,16 @@
+use std::io::{self, BufRead};
+
+/*
+ * Complete the 'countResponseTimeRegressions' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts INTEGER_ARRAY responseTimes as parameter.
+ */
+
+fn countResponseTimeRegressions(responseTimes: &[i32]) -> i32 {
+
+}
+
+fn average(previous_resp_times: &[32]) -> i32 {
+
+}
