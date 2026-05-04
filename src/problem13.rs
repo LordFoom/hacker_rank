0519b816d767066431d3c3a6dc5e914bf3858806
LordFoom <lordfoom@gmail.com>
2026-05-04 21:15:04 +0200

    Naive implementation

--- src/problem13.rs
+++ src/problem13.rs
@@ -3,9 +3,50 @@
 
 */
 fn findTaskPairForSlot(taskDurations: &[i32], slotLength: i32) -> Vec<i32> {
+    if taskDurations.is_empty() {
+        return vec![-1, -1];
+    }
     for i in 0..taskDurations.len() - 1 {
-        for j in i + 1..taskDurations.len() - 1 {}
+        for j in i + 1..taskDurations.len() {
+            let num1 = taskDurations[i];
+            let num2 = taskDurations[j];
+            if num1 + num2 == slotLength {
+                return vec![i as i32, j as i32];
+            }
+        }
     }
 
     vec![-1, -1]
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    pub fn simple_test_find_early_vec() {
+        let test_vec = vec![1, 2, 3, 4];
+        let res = findTaskPairForSlot(&test_vec, 3);
+        assert_eq!(vec![0, 1], res);
+    }
+
+    #[test]
+    pub fn simple_test_empty() {
+        let res = findTaskPairForSlot(&vec![], 4);
+        assert_eq!(vec![-1, -1], res);
+    }
+
+    #[test]
+    pub fn simple_test_end_vec() {
+        let test_vec = vec![1, 2, 3, 4, 5, 6, 100];
+        let res = findTaskPairForSlot(&test_vec, 106);
+        assert_eq!(vec![5, 6], res);
+    }
+
+    #[test]
+    pub fn simple_test_start_end_vec() {
+        let test_vec = vec![1, 2, 3, 4, 5, 6, 100];
+        let res = findTaskPairForSlot(&test_vec, 101);
+        assert_eq!(vec![0, 6], res);
+    }
 }
