4e457e6d4082688e11d8cf9c15a6598deddb65f0
lordfoom <lordfoom@gmail.com>
2026-07-09 21:19:13 +0200

    Add another diagonal scenario

--- src/problem19.rs
+++ src/problem19.rs
@@ -161,8 +161,15 @@ fn is_clear_diagonal(x: usize, y: usize, grid: &[Vec<i32>]) -> bool {
     }
     //( x+1, y-1 ), (x+2, y-2) down to 0 and x<grid[i].len()
     for i in (x..grid[0].len()) {
-           continue;
-       } 
+        if i == x {
+            continue;
+        }
+        let row = &grid[i];
+        for j in (0..y).rev() {
+            if row[j] == -1 {
+                return false;
+            }
+        }
     }
     //symmetrically ( x-1, y-1 ),(x-2, y-1) etc
     for i in (0..x).rev() {}
