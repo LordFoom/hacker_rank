5149f0d2ed6bd77a68f59ebce984e764c12f293c
lordfoom <lordfoom@gmail.com>
2026-07-20 21:53:10 +0200

    Some initial slightly exploratory coding

--- src/problem22.rs
+++ src/problem22.rs
@@ -16,7 +16,18 @@ fn deleteDuplicates(head: *const SinglyLinkedListNode) -> *const SinglyLinkedLis
     let mut i = 0;
     let mut first_pointer = head;
     let mut second_pointer = head;
+    let mut is_first_step = true;
     while !second_pointer.is_null() {
+        let second_pointer=second_pointer.next;
+        if is_first_step {
+            is_first_step=false;
+            continue;
+        }
+        let first_pointer=first_pointer.next;
+        //we cut out the duplicates by pointing away from them
+        while !second_pointer.is_null && first_pointer.data == second_pointer.data {
+            second_pointer = second_pointer.next;
+        }
 
     }
 
