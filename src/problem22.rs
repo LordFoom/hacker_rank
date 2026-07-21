e6af49cfaa3f3aacb84b74d9fe3983d1923363ff
lordfoom <lordfoom@gmail.com>
2026-07-21 22:13:07 +0200

    Simpler strategy
    So much simpler, wow. Now to get pointer dereferencing right

--- src/problem22.rs
+++ src/problem22.rs
@@ -6,29 +6,20 @@
  * The function accepts INTEGER_SINGLY_LINKED_LIST head as parameter.
  */
 
- SinglyLinkedListNode {
+ struct SinglyLinkedListNode {
      data: i32,
      next: *mut SinglyLinkedListNode,
- };
+ }
 
 fn deleteDuplicates(head: *const SinglyLinkedListNode) -> *const SinglyLinkedListNode {
-    // Write your code here
-    let mut i = 0;
     let mut first_pointer = head;
-    let mut second_pointer = head;
-    let mut is_first_step = true;
-    while !second_pointer.is_null() {
-        let second_pointer=second_pointer.next;
-        if is_first_step {
-            is_first_step=false;
-            continue;
+    unsafe{
+        while !(*first_pointer).next.is_null() {
+            if (*first_pointer).next.data == (*first_pointer).data {
+                (*first_pointer).next=(*first_pointer).next.next
+            }
         }
-        let first_pointer=first_pointer.next;
-        //we cut out the duplicates by pointing away from them
-        while !second_pointer.is_null && first_pointer.data == second_pointer.data {
-            second_pointer = second_pointer.next;
-        }
-
     }
+    head
 
 }
