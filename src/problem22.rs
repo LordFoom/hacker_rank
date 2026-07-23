b6d62a71239b6e21cdbccf16bf1f7a13b490cda0
lordfoom <lordfoom@gmail.com>
2026-07-23 21:50:26 +0200

    Solve it
    all that unsafe makes me feel unsafe

--- src/problem22.rs
+++ src/problem22.rs
@@ -12,12 +12,17 @@
  }
 
 fn deleteDuplicates(head: *const SinglyLinkedListNode) -> *const SinglyLinkedListNode {
-    let mut first_pointer = head;
+    let mut first_pointer: *mut SinglyLinkedListNode = head as *mut _;
     unsafe{
-        while !(*first_pointer).next.is_null() {
-            if (*first_pointer).next.data == (*first_pointer).data {
-                (*first_pointer).next=(*first_pointer).next.next
+        while !first_pointer.is_null() && !(*first_pointer).next.is_null() {
+            if (* (*first_pointer).next).data  == (*first_pointer).data {
+                (*first_pointer).next=(*(*first_pointer).next).next;
+                continue;
             }
+            if (*first_pointer).next.is_null(){
+                continue;
+            }
+            first_pointer=(*first_pointer).next;
         }
     }
     head
