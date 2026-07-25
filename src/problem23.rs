e09f13be74b40d52a52b571bdde722fe70097294
lordfoom <lordfoom@gmail.com>
2026-07-25 23:23:29 +0200

    Build adjacency list

--- src/problem23.rs
+++ src/problem23.rs
@@ -1,6 +1,13 @@
 fn hasCircularDependency(n: i32, dependencies: &[Vec<i32>]) -> bool {
 
-    let mut adjency_list = vec![Vec::new(); n as usize];
+    let mut adjacency_list = vec![Vec::new(); n as usize];
+    for dependency in dependencies {
+        let a = dependency[a];
+        let b = dependency[b];
+
+        adjacency_list[a].push(b);
+        adjacency_list[b].push(a);
+    }
     
 
     false
