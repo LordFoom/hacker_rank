2b19368341e48cbdc8eb1f404cc08d7a36dc7b13
LordFoom <lordfoom@gmail.com>
2026-07-28 20:41:14 +0200

    Some uncompiling thoughts from work

--- src/problem23.rs
+++ src/problem23.rs
@@ -2,15 +2,26 @@ fn hasCircularDependency(n: i32, dependencies: &[Vec<i32>]) -> bool {
 
     let mut adjacency_list = vec![Vec::new(); n as usize];
     for dependency in dependencies {
-        let a = dependency[a];
-        let b = dependency[b];
+        let a = dependency[0];
+        let b = dependency[1];
 
         adjacency_list[a].push(b);
-        adjacency_list[b].push(a);
+    }
+
+    let mut state = vec![0; n as usize];
+
+    for node in 0..n as usize {
+        if state[node]=0 && has_cycle(node, &adjecency_list,&mut state){
+
+        }
     }
     
 
     false
 
+}
+
+fn has_cycle(node: usize, adjacency_list: &[Vec<usize>], state: &mut [u8] ){
+    state[node]=1;
 }
 
